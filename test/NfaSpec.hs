21f44409ae345ba16d65af4e8138b9a229faea79
David Kofler <kofler.david@gmail.com>
2016-09-18 17:37:13 +0200

    Expand tests for NFA

--- test/NfaSpec.hs
+++ test/NfaSpec.hs
@@ -1,5 +1,6 @@
 module NfaSpec (main, spec) where
 
+import Control.Monad (forM_)
 import Test.Hspec
 import Test.QuickCheck
 
@@ -14,12 +15,39 @@ spec :: Spec
 spec = do
   describe "nfa for \"\"" $ do
     let nfa = Nfa ['a'] [0] [0] [0] []
-    it "should accept \"\"" $ do
-      nfa `accepts` runNfa nfa "" `shouldBe` True
+
+    it "should accept \"\"" $
+      nfa `shouldAccept` ""
+
     it "should not accept \"a\"" $ do
-      nfa `accepts` runNfa nfa "a" `shouldBe` False
+      nfa `shouldNotAccept` "a"
 
   describe "nfa for 'a'" $ do
     let nfa = Nfa ['a'] [0, 1] [0] [1] [((0, Just 'a'), [1])]
-    it "accepts \"a\"" $ do
-      nfa `accepts` runNfa nfa "a" `shouldBe` True
+
+    it "should accept \"a\"" $ do
+      nfa `shouldAccept` "a"
+
+    forM_ ["", "aa", "b", "ab", "ba"] $ \input -> do
+      it ("should not accept " ++ show input) $
+        nfa `shouldNotAccept` input
+
+  describe "nfa for a?b" $ do
+    let nfa = Nfa ['a', 'b'] [0, 1, 2, 3] [0] [3] [((0, Just 'a'), [1]),
+                                                   ((0, Nothing), [2]),
+                                                   ((1, Just 'b'), [3]),
+                                                   ((2, Just 'b'), [3])]
+
+    forM_ ["b", "ab"] $ \input -> do
+      it ("should accept " ++ show input) $ do
+        nfa `shouldAccept` input
+
+    forM_ ["", "aa", "ba", "bb", "c", "bc", "abc"] $ \input -> do
+      it ("should not accept " ++ show input) $
+        nfa `shouldNotAccept` input
+
+shouldAccept :: (Ord c, Show c) => Nfa c -> [c] -> Expectation
+shouldAccept nfa input = runNfa nfa input `shouldSatisfy` (nfa `accepts`)
+
+shouldNotAccept :: (Ord c, Show c) => Nfa c -> [c] -> Expectation
+shouldNotAccept nfa input = runNfa nfa input `shouldNotSatisfy` (nfa `accepts`)
