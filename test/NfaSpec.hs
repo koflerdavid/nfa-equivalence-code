7d0172c46eeb011efbc213a82df2516f96b331e8
David Kofler <kofler.david@gmail.com>
2016-09-18 12:54:04 +0200

    Add some tests for NFAs

--- test/NfaSpec.hs
+++ test/NfaSpec.hs
@@ -11,4 +11,15 @@ main :: IO ()
 main = hspec spec
 
 spec :: Spec
-spec = return ()
+spec = do
+  describe "nfa for \"\"" $ do
+    let nfa = Nfa ['a'] [0] [0] [0] []
+    it "should accept \"\"" $ do
+      nfa `accepts` runNfa nfa "" `shouldBe` True
+    it "should not accept \"a\"" $ do
+      nfa `accepts` runNfa nfa "a" `shouldBe` False
+
+  describe "nfa for 'a'" $ do
+    let nfa = Nfa ['a'] [0, 1] [0] [1] [((0, Just 'a'), [1])]
+    it "accepts \"a\"" $ do
+      nfa `accepts` runNfa nfa "a" `shouldBe` True
