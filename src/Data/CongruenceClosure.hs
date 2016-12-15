48121928ed8a8aff5f7c0da5b973619c512cd84f
David Kofler <kofler.david@gmail.com>
2016-12-15 01:44:15 +0100

    Refactor congruence closure module.

--- src/Data/CongruenceClosure.hs
+++ src/Data/CongruenceClosure.hs
@@ -2,45 +2,25 @@ module Data.CongruenceClosure
     ( CongruenceClosure
     , empty
     , equate
-    , equal
+    , equivalent
     ) where
+
+import Data.CongruenceClosure.Internal
 
 import qualified Data.IntSet as IS
 
 type Rule = (IS.IntSet, IS.IntSet)
-
 data CongruenceClosure = CongruenceClosure [Rule]
 
 empty :: CongruenceClosure
 empty = CongruenceClosure []
 
-equate :: CongruenceClosure -> [Int] -> [Int] -> CongruenceClosure
-equate rel is1 is2 = equateIntSets rel (IS.fromList is1) (IS.fromList is2)
-
-equateIntSets :: CongruenceClosure -> IS.IntSet -> IS.IntSet -> CongruenceClosure
-equateIntSets relation@(CongruenceClosure rules) iset1 iset2 =
-    if intSetsEqual iset1 iset2 relation
+equate :: CongruenceClosure -> IS.IntSet -> IS.IntSet -> CongruenceClosure
+equate relation@(CongruenceClosure rules) iset1 iset2 =
+    if equivalent iset1 iset2 relation
     then relation
     else CongruenceClosure ((iset1, iset2) : (iset2, iset1) : rules)
 
-equal :: [Int] -> [Int] -> CongruenceClosure -> Bool
-equal is1 is2 rel = intSetsEqual (IS.fromList is1) (IS.fromList is2) rel
-
-intSetsEqual :: IS.IntSet -> IS.IntSet -> CongruenceClosure -> Bool
-intSetsEqual is1 is2 rel =
-    normalForm rel is1 == normalForm rel is2
-
-normalForm :: CongruenceClosure -> IS.IntSet -> IS.IntSet
-normalForm (CongruenceClosure rules) integerSet =
-    normalise integerSet rules [] False
-  where
-    normalise iset [] [] _ =
-        iset
-    normalise iset [] _toRetry False =
-        iset
-    normalise iset [] toRetry True =
-        normalise iset toRetry [] False
-    normalise iset (rule@(left, right) : rs) unmatched retry =
-        if left `IS.isSubsetOf` iset
-        then normalise (iset `IS.union` right) rs unmatched True
-        else normalise iset rs (rule : unmatched) retry
+equivalent :: IS.IntSet -> IS.IntSet -> CongruenceClosure -> Bool
+equivalent is1 is2 (CongruenceClosure rules) =
+    normalForm rules is1 == normalForm rules is2
--- test/Data/CongruenceClosureSpec.hs
+++ test/Data/CongruenceClosureSpec.hs
@@ -3,9 +3,10 @@ module Data.CongruenceClosureSpec
     , spec
     ) where
 
-import           Test.Hspec
+import qualified Data.CongruenceClosure as CC
 
-import           Data.CongruenceClosure as SIS
+import qualified Data.IntSet            as IS
+import           Test.Hspec
 
 main :: IO ()
 main = hspec spec
@@ -14,45 +15,51 @@ spec :: Spec
 spec = do
     describe "empty family of sets" $ do
         it "should consider equal sets as equal" $ do
-            let emptySetOfSets = SIS.empty
-            SIS.equal [ 1 ] [ 1 ] emptySetOfSets `shouldBe` True
+            let emptySetOfSets = CC.empty
+            equal [ 1 ] [ 1 ] emptySetOfSets `shouldBe` True
 
         it "should not consider any sets as equal" $ do
-            let emptySetOfSets = SIS.empty
-            SIS.equal [ 2 ] [ 3 ] emptySetOfSets `shouldBe` False
+            let emptySetOfSets = CC.empty
+            equal [ 2 ] [ 3 ] emptySetOfSets `shouldBe` False
 
-    let relation = SIS.equate SIS.empty [ 1 ] [ 2 ]
+    let relation = equate CC.empty [ 1 ] [ 2 ]
 
     describe "equivalence relation on sets with {1} = {2}" $ do
         it "should consider {1} and {2} as equal" $ do
-            SIS.equal [ 1 ] [ 2 ] relation `shouldBe` True
+            equal [ 1 ] [ 2 ] relation `shouldBe` True
 
         it "should consider {1} and {1} as equal" $ do
-            SIS.equal [ 1 ] [ 1 ] relation `shouldBe` True
+            equal [ 1 ] [ 1 ] relation `shouldBe` True
 
         it "should consider {1} and {1, 2} as equal" $ do
-            SIS.equal [ 1 ] [ 1, 2 ] relation `shouldBe` True
+            equal [ 1 ] [ 1, 2 ] relation `shouldBe` True
 
         it "should not consider {1} and {} as equal" $ do
-            SIS.equal [ 1 ] [] relation `shouldBe` False
+            equal [ 1 ] [] relation `shouldBe` False
 
         it "should not consider {1} and {3} as equal" $ do
-            SIS.equal [ 1 ] [ 3 ] relation `shouldBe` False
+            equal [ 1 ] [ 3 ] relation `shouldBe` False
 
         it "should not consider {1, 3} and {3} as equal" $ do
-            SIS.equal [ 1, 3 ] [ 3 ] relation `shouldBe` False
+            equal [ 1, 3 ] [ 3 ] relation `shouldBe` False
 
         it "should not consider {1, 3} and {4} as equal" $ do
-            SIS.equal [ 1, 3 ] [ 4 ] relation `shouldBe` False
+            equal [ 1, 3 ] [ 4 ] relation `shouldBe` False
 
     describe "equivalence relation on sets with {1} = {2} and {3, 4} = {3, 5}" $ do
-        let relation' = SIS.equate relation [ 3, 4 ] [ 3, 5 ]
+        let relation' = equate relation [ 3, 4 ] [ 3, 5 ]
 
         it "should consider {3, 4} and {3, 5} as equal" $ do
-            SIS.equal [ 3, 4 ] [ 3, 5 ] relation' `shouldBe` True
+            equal [ 3, 4 ] [ 3, 5 ] relation' `shouldBe` True
 
         it "should consider {1, 3, 4} and {2, 3, 5} as equal" $ do
-            SIS.equal [ 1, 3, 4 ] [ 2, 3, 5 ] relation' `shouldBe` True
+            equal [ 1, 3, 4 ] [ 2, 3, 5 ] relation' `shouldBe` True
 
         it "should not consider {2, 3, 4} and {3, 5} as equal" $ do
-            SIS.equal [ 2, 3, 4 ] [ 3, 5 ] relation' `shouldNotBe` True
+            equal [ 2, 3, 4 ] [ 3, 5 ] relation' `shouldNotBe` True
+
+equal :: [Int] -> [Int] -> CC.CongruenceClosure -> Bool
+equal is1 is2 rel = CC.equivalent (IS.fromList is1) (IS.fromList is2) rel
+
+equate :: CC.CongruenceClosure -> [Int] -> [Int] -> CC.CongruenceClosure
+equate rel is1 is2 = CC.equate rel (IS.fromList is1) (IS.fromList is2)
