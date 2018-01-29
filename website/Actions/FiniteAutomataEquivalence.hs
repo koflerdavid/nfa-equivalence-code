f15f25bfed9f30e0816dbd5a7973cfc4dbb785ed
David Kofler <kofler.david@gmail.com>
2018-01-29 13:28:47 +0100

    Display the execution trace for automata equivalence checking

--- website/Actions/FiniteAutomataEquivalence.hs
+++ website/Actions/FiniteAutomataEquivalence.hs
@@ -8,8 +8,9 @@ import           Data.Aeson.Text              ( encodeToLazyText )
 import           Data.ByteString.Lazy         as LBS
 import           Data.ByteString.UTF8         as UTF8
 import           Data.Either.Combinators      ( mapLeft )
-import           Data.IntSet                  ( fromList )
+import           Data.IntSet                  ( fromList, toList )
 import           Data.Map                     ( Map, lookup )
+import           Data.Maybe                   ( fromJust )
 import           Data.Monoid                  ( (<>) )
 import qualified Data.Text.Lazy               as T
 import           Snap.Core
@@ -25,14 +26,37 @@ data EquivalenceError
     | NoCheckSpecified
     | CheckedStateDoesNotExist String
 
+data PrettyConstraint =
+    PrettyConstraint String -- | The input that lead to this constraint
+                     [String] -- | The first set of states
+                     [String] -- | The second set of states
+
+-- | A trace is the execution sequence of the algorithm
+-- Constraints are either ignored or are significant.
+data Trace = Trace
+    { stateSetChecked    :: Bool
+    , stateSetConstraint :: PrettyConstraint
+    }
+
+-- | A witness is a string which is accepted by one state set, but not by another
+type Witness = PrettyConstraint
+
 data EquivalenceResult
-    = Equivalent
-    | NotEquivalent [String]
+    = Equivalent [Trace]
+    | NotEquivalent Witness
+                    [Trace]
 
 instance ToJSON EquivalenceResult where
-    toJSON Equivalent = object ["equivalent" .= True]
-    toJSON (NotEquivalent witnesses) =
-        object ["equivalent" .= False, "witnesses" .= witnesses]
+    toJSON (Equivalent trace) = object ["equivalent" .= True, "trace" .= trace]
+    toJSON (NotEquivalent witness trace) =
+        object ["equivalent" .= False, "witnesses" .= [witness], "trace" .= trace]
+
+instance ToJSON Trace where
+    toJSON (Trace checked constraint) = object ["checked" .= checked, "constraint" .= constraint]
+
+instance ToJSON PrettyConstraint where
+    toJSON (PrettyConstraint input stateSet1 stateSet2) =
+        object ["input" .= input, "stateSet1" .= stateSet1, "stateSet2" .= stateSet2]
 
 action :: Snap ()
 action =
@@ -48,7 +72,7 @@ action =
                     Utf8DecodeError -> writeLazyText "The request contains invalid UTF8 codepoints"
                     ParseError syntaxError ->
                         writeLazyText $
-                            "The request contains a syntax error:\n" <> T.pack syntaxError
+                        "The request contains a syntax error:\n" <> T.pack syntaxError
                     NoCheckSpecified ->
                         writeLazyText "The input does not contain a constraint to check for"
                     CheckedStateDoesNotExist state ->
@@ -64,11 +88,9 @@ equivalent (Just utf8Body)
  = do
     body <- maybe (Left Utf8DecodeError) Right (fromUtf8 . LBS.toStrict $ utf8Body)
     -- Parse body
-    Result transitions acceptingStates checks <-
-        mapLeft ParseError $ parseHknt body
+    Result transitions acceptingStates checks <- mapLeft ParseError $ parseHknt body
     -- Wrap errors into ParseError
-    (nfa, stateMapping) <-
-        mapLeft ParseError $ compileHkntToNfa transitions acceptingStates
+    (nfa, stateMapping) <- mapLeft ParseError $ compileHkntToNfa transitions acceptingStates
     -- Ensure there is at least one check
     when (Prelude.null checks) $ Left NoCheckSpecified
     -- Only consider the last constraint. Assume it is an equivalence constraint
@@ -77,14 +99,16 @@ equivalent (Just utf8Body)
     stateSet1' <- mapM (translateState stateMapping) stateSet1
     stateSet2' <- mapM (translateState stateMapping) stateSet2
     -- Determine equivalence
-    let (maybeWitness, _) =
+    let (maybeWitness, trace) =
             nfaStatesDifferencesHkC
                 nfa
                 (Data.IntSet.fromList stateSet1')
                 (Data.IntSet.fromList stateSet2')
+        invStateMapping = invertedStateMapping stateMapping
+        trace' = Prelude.map (translatedTrace invStateMapping) trace
     case maybeWitness of
-        Nothing              -> return Equivalent
-        Just (witness, _, _) -> return (NotEquivalent [witness])
+        Nothing -> return (Equivalent trace')
+        Just witness -> return (NotEquivalent (translatedConstraint invStateMapping witness) trace')
 
 fromUtf8 :: UTF8.ByteString -> Maybe String
 fromUtf8 bs = do
@@ -93,9 +117,30 @@ fromUtf8 bs = do
         then Nothing
         else Just str
 
+-- | Translate a set of user input states to the internal states needed for computing equivalence
+-- The user could have specified nonexisting states, so we have to watch out for that
 translateState :: (Ord s, Show s) => Map s s' -> s -> Either EquivalenceError s'
 translateState stateMapping state =
     maybe
         (Left . CheckedStateDoesNotExist . show $ state)
         return
         (state `Data.Map.lookup` stateMapping)
+
+translatedTrace :: Map Int String -> (Bool, Constraint Char) -> Trace
+translatedTrace invStateMapping (considered, constraint) =
+    Trace
+    { stateSetChecked = considered
+    , stateSetConstraint = translatedConstraint invStateMapping constraint
+    }
+
+-- | Map back state numbers to the state names specified by the user
+-- There will be no check whether the state numbers are valid because they are supposed to come from the compiler.
+translatedConstraint :: Map Int String -> Constraint Char -> PrettyConstraint
+translatedConstraint invStateMapping (input, stateSet1, stateSet2) =
+    let prettyStateSet1 =
+            Prelude.map (fromJust . (`Data.Map.lookup` invStateMapping)) . Data.IntSet.toList $
+            stateSet1
+        prettyStateSet2 =
+            Prelude.map (fromJust . (`Data.Map.lookup` invStateMapping)) . Data.IntSet.toList $
+            stateSet2
+    in PrettyConstraint input prettyStateSet1 prettyStateSet2
