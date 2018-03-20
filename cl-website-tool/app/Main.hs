dc467b4f9fd583c55f4737b0e42751f2a961c41f
David Kofler <kofler.david@gmail.com>
2018-03-20 18:39:54 +0100

    Use a Cabal feature to locate the website assets.
    See the
    [documentation](https://www.haskell.org/cabal/users-guide/developing-packages.html#package-properties)
    of the `data-files` property and
    [here](https://www.haskell.org/cabal/users-guide/developing-packages.html#accessing-data-files-from-package-code)
    on how to access them.

--- cl-website-tool/app/Main.hs
+++ cl-website-tool/app/Main.hs
@@ -1,11 +1,14 @@
 module Main where
 
+import Paths_cl_website_tool             ( getDataFileName )
+
 import Actions.Derivation                as Derivation
 import Actions.FiniteAutomataEquivalence as FiniteAutomataEquivalence
 import Actions.RegexEquivalence          as RegexEquivalence
 import Actions.RegexToDfaConversion      as RegexToDfaConversion
 
 import Control.Applicative               ( (<|>) )
+import Control.Monad.IO.Class            ( liftIO )
 import Snap.Core
 import Snap.Http.Server
 import Snap.Util.FileServe
@@ -14,16 +17,20 @@ main :: IO ()
 main = quickHttpServe site
 
 site :: Snap ()
-site =
-    ifTop (modifyResponse (setContentType "text/html; charset=utf-8") >> sendFile "static/index.html") <|>
-    route
-        [ ("finiteAutomata/equivalence", FiniteAutomataEquivalence.action)
-        , ("regex/derivation", Derivation.action)
-        , ("regex/dfa_conversion", RegexToDfaConversion.action)
-        , ("regex/equivalence", RegexEquivalence.action)
-        ] <|>
-    dir "static" (serveDirectory "static") <|>
-    notFoundError
+site = ifTop index
+   <|> route
+       [ ("finiteAutomata/equivalence", FiniteAutomataEquivalence.action)
+       , ("regex/derivation", Derivation.action)
+       , ("regex/dfa_conversion", RegexToDfaConversion.action)
+       , ("regex/equivalence", RegexEquivalence.action)
+       ]
+   <|> dir "static" (serveDirectory =<< liftIO (getDataFileName "static"))
+   <|> notFoundError
+
+index :: Snap ()
+index = do
+    modifyResponse $ setContentType "text/html; charset=utf-8"
+    sendFile =<< liftIO (getDataFileName "static/index.html")
 
 notFoundError :: Snap ()
 notFoundError = do
